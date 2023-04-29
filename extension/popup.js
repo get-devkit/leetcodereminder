a58b6a13aeb618c460131adc6b54528b44816819
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-04-29 15:10:04 +0530

    Updated haveUserInfo function

--- extension/popup.js
+++ extension/popup.js
@@ -4,8 +4,10 @@ main() // calling main function
 async function main(){
 
     
-    //Check whether we have info for Leetcode profile    
-    if( !(haveUserInfo()) ) handleLoginComponent()
+    //Check whether we have info for Leetcode profile
+    let haveInfo = await haveUserInfo();
+
+    if( !haveInfo ) loginPrompt()
 
 }
 
@@ -14,13 +16,30 @@ async function main(){
 //Returns true if we have user info otherwise false
 function haveUserInfo(){
 
-    return false
+    return new Promise( async (resolve , reject )=>{
+  
+        try{
+
+            //Check if we have userInfo in chrome storage
+            let userInfo = await chrome.storage.local.get('userInfo')
+            
+            if( userInfo.userInfo === undefined ) resolve(false) ;
+            else  resolve(true)
+
+        }catch(err) {
+
+            reject(err)
+
+        }
+        
+    })
 
 }
 
 
 //Returns HTML Component for prompting user to open leetcode profile page
-function handleLoginComponent (){
+function loginPrompt(){
+
 
     let comp = document.createElement('p')
     comp.innerHTML = "Please open your leetcode profile !! &#10024;"
