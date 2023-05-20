be67e1bfe4125cdab7ad506cd7bdb25718f58d0c
Bhavesh Anandpara <87887912+BhaveshAnandpara@users.noreply.github.com>
2023-05-20 22:54:44 +0530

    Authentication and Popup Funcationality (#6)

---
+++ extension/background.js
@@ -0,0 +1,67 @@
+//*----------------------------- Variables ------------------------------------ *//
+
+const serverProxy = 'https://leetcodereminder.vercel.app/api'
+
+
+//Checking Whether the Tab is Leetcode Profile and if yes then send the message to get the username
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+    chrome.tabs.get(tabId).then((result) => {
+
+        // console.log(result);
+
+        //if the the webpage is leetcode profile page and loading is complete
+        if (result.url === "https://leetcode.com/profile/" && result.status === 'complete') {
+
+            //Send Message to content script that page is loded
+            chrome.tabs.sendMessage(result.id, { loaded: true })
+        }
+
+    }).catch((err) => {
+        console.log(err);
+    });
+
+});
+
+
+// Recieve Messages
+chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
+
+    //Checks if we need to get userInfo or not
+    if (req.userInfo) {
+
+        // console.log(JSON.stringify({ username: req.username }));
+
+
+        //* Calling API to getUserInfo *//
+
+        // Get User Details
+        const response = await fetch(`${serverProxy}/getUserDetails`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: req.username })
+        }).catch((err) => {
+            console.log(err);
+        })
+
+        //Save User data in chrome.storage
+        await response.json().then((res) => {
+
+            // console.log(res);
+
+            //Store UserInfo in chrome local storage
+            chrome.storage.local.set({ userInfo: res }, () => {
+
+                console.log("UserInfo Stored");
+
+            })
+
+        })
+
+    }
+
+    return true;
+})
+
---
+++ extension/content.js
@@ -0,0 +1,50 @@
+
+
+// Recieve Message 
+chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
+
+    //Checks if Page is loaded or not
+    if (req.loaded) {
+
+        await getUsername().then(async (result) => {
+
+            //Save username in chrome's localstoarge
+            await chrome.storage.local.set({ username: result.username }, () => {
+                console.log("Username Stored");
+            })
+
+            //Send Message to get the userInfo
+            await chrome.runtime.sendMessage(sender.id, { userInfo: true, username: result.username })
+
+        }).catch((err) => {
+            //send Response as we need to reload again
+            console.log(err);
+        })
+    }
+
+    return true;
+
+})
+
+
+// Function returns username
+async function getUsername() {
+
+    return new Promise((resolve, reject) => {
+
+        const body = document.querySelector('body')
+
+        if (body) {
+
+            let usernameContainer = document.querySelector(".content-wrapper #base_content #profile-app .profile-app__KJyN .header__NyBc .container__28Ei .user-info__2aLr .leetcodeId__13b9");
+
+            // eg :  usernameContainer = " LeetCode ID: xyz "
+            let username = ((usernameContainer.textContent).split(':')[1]).trim()
+
+            if (username != null) resolve({ username, status: true })
+            reject({ status: false })
+        }
+
+
+    })
+}
