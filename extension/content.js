a6022de62f6d166f2d4fc2d4e11f9d119b5a98d0
Bhavesh Anandpara <87887912+BhaveshAnandpara@users.noreply.github.com>
2023-05-30 14:17:47 +0530

    Reminder Box Added (#6)
    * Added Functionality to get username from input box
    
    * Logic to extract username
    
    * Backend Updated
    
    * scripts updated
    
    * Logic to fetch user Details
    
    * fetch All the User Details and Show in popup
    
    * Save reminder Information
    
    * Loads user Details
    
    * gap beteen stats
    
    * API created to get random Easy Que title
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>

--- extension/content.js
+++ extension/content.js
@@ -1,8 +1,9 @@
 
 
-// Recieve Message that page is loaded
+// Recieve Message 
 chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
 
+    //Checks if Page is loaded or not
     if (req.loaded) {
 
         await getUsername().then(async (result) => {
@@ -12,7 +13,7 @@ chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
                 console.log("Username Stored");
             })
 
-            //Send Message that page is loded
+            //Send Message to get the userInfo
             await chrome.runtime.sendMessage(sender.id, { userInfo: true, username: result.username })
 
         }).catch((err) => {
@@ -26,7 +27,7 @@ chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
 })
 
 
-// Function that returns username
+// Function returns username
 async function getUsername() {
 
     return new Promise((resolve, reject) => {
@@ -37,7 +38,7 @@ async function getUsername() {
 
             let usernameContainer = document.querySelector(".content-wrapper #base_content #profile-app .profile-app__KJyN .header__NyBc .container__28Ei .user-info__2aLr .leetcodeId__13b9");
 
-            // eg : LeetCode ID: bhaveshanandpara12
+            // eg :  usernameContainer = " LeetCode ID: xyz "
             let username = ((usernameContainer.textContent).split(':')[1]).trim()
 
             if (username != null) resolve({ username, status: true })
