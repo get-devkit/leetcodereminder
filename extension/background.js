17e03b1f8a294d47bf4cfad3fd81cdbef541d0b1
Bhavesh Anandpara <87887912+BhaveshAnandpara@users.noreply.github.com>
2023-05-30 17:42:18 +0530

    Reminder Container, Logic and Cat Images (#8)
    * Readme.md file added
    
    * Update Readme.md
    
    * Update Readme.md
    
    * Update Readme.md
    
    * Popup Files are added
    
    * Updated haveUserInfo function
    
    * Popup Frontend is done
    
    * backend Added
    
    * getUserDetails API created
    
    * Added CORS in backend (#3)
    
    * Docs Added (#2)
    
    * Readme.md file added
    
    * Update Readme.md
    
    
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    * Added Functionality to get username from input box
    
    * Logic to extract username
    
    * Backend Updated
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    * Logic to get User Details (#4)
    
    * Docs Added (#2)
    
    * Readme.md file added
    
    * Update Readme.md
    
    
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    * Added Functionality to get username from input box
    
    * Logic to extract username
    
    * Backend Updated
    
    * scripts updated
    
    * Logic to fetch user Details
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    * Logic to get userdetails (#5)
    
    * Docs Added (#2)
    
    * Readme.md file added
    
    * Update Readme.md
    
    
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    * Added Functionality to get username from input box
    
    * Logic to extract username
    
    * Backend Updated
    
    * scripts updated
    
    * Logic to fetch user Details
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    * Reminder Box Added (#6)
    
    * Added Functionality to get username from input box
    
    * Logic to extract username
    
    * Backend Updated
    
    * scripts updated
    
    * Logic to fetch user Details
    
    * fetch All the User Details and Show in popup
    
    * Save reminder Information
    
    * Loads user Details
    
    * gap beteen stats
    
    * API created to get random Easy Que title
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    * Changes on Reminder Container (#7)
    
    * Added Functionality to get username from input box
    
    * Logic to extract username
    
    * Backend Updated
    
    * scripts updated
    
    * Logic to fetch user Details
    
    * fetch All the User Details and Show in popup
    
    * Save reminder Information
    
    * Loads user Details
    
    * gap beteen stats
    
    * API created to get random Easy Que title
    
    * Reminder Logic
    
    * Logic to check and show reminder on every page
    
    * Added Text and Cat Images
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,10 @@
-
 //*----------------------------- Variables ------------------------------------ *//
 
 const serverProxy = 'https://leetcodereminder.vercel.app/api'
+var popupInfo = {}; 
+var intialInterval = 30 * 1000 // 60 secs
+
+
 
 
 //Checking Whether the Tab is Leetcode Profile and if yes then send the message to get the username
@@ -20,6 +23,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
     }).catch((err) => {
         console.log(err);
+        chrome.runtime.reload()
+
     });
 
 });
@@ -30,9 +35,6 @@ chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
 
     //Checks if we need to get userInfo or not
     if (req.userInfo) {
-
-        // console.log(JSON.stringify({ username: req.username }));
-
 
         //* Calling API to getUserInfo *//
 
@@ -52,13 +54,16 @@ chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
         //Save User data in chrome.storage
         await response.json().then((res) => {
 
-          // console.log(res);
+
 
             //Store UserInfo in chrome local storage
             chrome.storage.local.set({ userInfo: res }, () => {
 
                 console.log("UserInfo Stored");
 
+            }).catch((err) => {
+                console.log(err);
+                chrome.runtime.reload()
 
             })
 
@@ -68,4 +73,114 @@ chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
 
     return true;
 })
+
+
+//Checking if Time is Right
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+
+    //For Activate Tabs
+    chrome.tabs.get(activeInfo.tabId).then(async (result) => {
+
+        //for that specific tab
+        popupInfo[activeInfo.tabId + ""] = false
+
+        //* SetInterval function to check time for every 5 sec
+
+        setInterval(async () => {
+
+            //! for debugging
+            console.log( intialInterval + " " + activeInfo.tabId + " " + popupInfo[activeInfo.tabId] );
+
+            let userInfo = await chrome.storage.local.get('userInfo')
+            userInfo = userInfo.userInfo
+
+            if (userInfo != undefined) {
+                //* check whether we should display reminder container
+                handleReminder(activeInfo.tabId)
+            }
+
+        }, intialInterval);
+
+
+
+    }).catch((err) => {
+        console.log(err);
+        chrome.runtime.reload()
+    });
+
+
+});
+
+
+//Function to check time for reminder and send necessary messages
+async function handleReminder(tabId) {
+
+    try {
+
+        let time = await chrome.storage.local.get('reminderTime')
+        time = time.reminderTime
+
+        let hr = parseInt(time.split(':')[0])
+        let min = parseInt(time.split(':')[1])
+
+        //Convert user set time into minutes
+        time = hr * 60 + min
+
+        //Converting Current Time in minutes
+        let now = new Date()
+        now = now.getHours() * 60 + now.getMinutes()
+
+
+        //If It's past the set time
+        if (now >= time) {
+
+            let interval = await chrome.storage.local.get('reminderInterval')
+            interval = interval.reminderInterval
+
+            //! For testing purpose only set it to 3 min ( otherwise 30 min )
+            if (interval === undefined) interval = 3
+
+            console.log("time remaining = " + (interval - (now % time) % interval));
+
+            //Only if it's time to show reminder
+            // popupInfo[tabId+""] to check for specific tabs
+            if ( (now % time) % interval == 0 && !popupInfo[tabId+""] ) {
+
+                console.log("sending msg to show the container");
+                
+                //* Sending the msg to content-scripts that show the reminder container
+                await chrome.tabs.sendMessage(tabId, { showReminder: true })
+
+                //* below sendMessage cause if errror occures we can redo this step otherwise it will be true and won't execute
+                popupInfo[tabId+""] = true
+                intialInterval = 30 * 1000
+                
+            }
+            
+            // If It's 1 minute past reminder
+            else if ((now % time) % interval != 0 && popupInfo[tabId+""]) {
+
+                intialInterval = (interval / 4) * 60 * 1000 //Checks for 4 times between interval
+                
+                console.log("Sending Msg to hide the container");
+                
+                //* Sending the msg to content-scripts that hide the reminder container
+                await chrome.tabs.sendMessage(tabId, { showReminder: false })
+
+                //* below sendMessage cause if errror occures we can redo this step otherwise it will be true and won't execute
+                popupInfo[tabId+""] = false
+
+            }
+
+        }
+
+    } catch (err) {
+
+        console.log(err);
+        chrome.runtime.reload()
+
+    }
+
+
+}
 
