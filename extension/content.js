667e818739a3deaa5c9313724cb26c7d0bbb3992
Bhavesh Anandpara <87887912+BhaveshAnandpara@users.noreply.github.com>
2023-05-30 17:36:19 +0530

    Changes on Reminder Container (#7)
    * Added Functionality to get username from input box
    
    * Logic to extract username
    
    * Backend Updated
    
    * scripts updated
    
    * Logic to fetch user Details
    
    * fetch All the User Details and Show in popup
    
    * Save reminder Information
    
    * Loads user Details
    
    * gap beteen stats
    
    * API created to get random Easy Que title
    
    * Reminder Logic
    
    * Logic to check and show reminder on every page
    
    * Added Text and Cat Images
    
    ---------
    
    Co-authored-by: Bhavesh Anandpara <bhaveshanandpara12@gmail.com>

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,13 @@
 
+//*----------------------------- Variables ------------------------------------ *//
+
+const body = document.querySelector("body");
+const serverProxy = 'https://leetcodereminder.vercel.app/api'
+
+
+
+//main function to create reminder
+main()
 
 // Recieve Message 
 chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
@@ -22,10 +31,168 @@ chrome.runtime.onMessage.addListener(async (req, sender, sendResponse) => {
         })
     }
 
+    if (req.showReminder) {
+        showReminder()
+    }
+
+    if (!req.showReminder) {
+        hideReminder()
+    }
+
     return true;
 
 })
 
+
+//Main Function 
+
+async function main() {
+
+    // `document.querySelector` may return null if the selector doesn't match anything.
+    if (body) {
+
+        //Creating Container
+        const box = document.createElement('div')
+        box.className = "leetcodeBox"
+        box.id = "leetcodeBox"
+
+        //Styling Container
+        box.style.display = "block"
+        box.style.width = "500px"
+        box.style.height = "3.4rem"
+        box.style.overflow = "hidden"
+        box.style.marginTop = "0rem !important"
+        box.style.backgroundColor = "rgba(32,32,32,0.5)"
+        box.style.zIndex = "10000"
+        box.style.position = "fixed"
+        box.style.borderRadius = "8px"
+        box.style.top = "10px"
+        box.style.right = "10px"
+        box.style.padding = "1rem 20px"
+        box.style.color = "#fff"
+        box.style.fontSize = "1rem"
+        box.style.pointerEvents = "none"
+        box.style.transform = "translate(120%)"
+        box.style.transition = "transform 1.8s ease-out"
+        box.style.backdropFilter = "blur(60px)"
+
+
+        //Creating header
+        const header = document.createElement('div')
+        header.className = "header"
+        header.style.display = "flex"
+        header.style.alignItems = "center"
+        header.style.justifyContent = "space-between"
+        header.style.flexDirection = "row"
+        header.style.marginBottom = "1rem"
+
+        //Creating header
+        const notification = document.createElement('div')
+        notification.className = "notification"
+        notification.style.display = "flex"
+        notification.style.alignItems = "center"
+        notification.style.flexDirection = "row"
+
+        //Creating Title
+        const title = document.createElement('p')
+        title.innerHTML = "&#10024; Leetcode Daily Reminder"
+        title.style.margin = "0"
+        title.style.fontSize = '1rem'
+        title.style.marginRight = "15px"
+
+
+        //Creating dropdown
+        const dropIcon = document.createElement('img')
+        const IconSrc = await chrome.runtime.getURL("icons/chevron-down.svg")
+        dropIcon.src = IconSrc
+        dropIcon.alt = "icons"
+        dropIcon.style.width = "1rem"
+        dropIcon.style.height = "1rem"
+        dropIcon.style.cursor = "pointer"
+        dropIcon.style.pointerEvents = "auto"
+        dropIcon.onclick = () => {
+
+            let box = document.getElementById('leetcodeBox')
+
+            if (box.style.height !== 'fit-content')
+                box.style.height = 'fit-content'
+            else {
+                box.style.height = '4rem'
+
+            }
+
+        }
+
+        notification.appendChild(title)
+        notification.appendChild(dropIcon)
+
+
+        //Creating close button
+        const closeIcon = document.createElement('img')
+        const closeIconSrc = await chrome.runtime.getURL("icons/x.svg")
+        closeIcon.src = closeIconSrc
+        closeIcon.alt = "close"
+        closeIcon.style.width = "1rem"
+        closeIcon.style.height = "1rem"
+        closeIcon.style.cursor = "pointer"
+        closeIcon.style.pointerEvents = "auto"
+        closeIcon.onclick = () => {
+
+            hideReminder();
+
+        }
+
+
+        header.appendChild(notification)
+        header.appendChild(closeIcon)
+
+
+        //create div tag whom bg image will have cat image
+        const imgDiv = document.createElement('div')
+        imgDiv.id = 'catImgDiv'
+        imgDiv.style.width = "100%"
+        imgDiv.style.display = "flex"
+        imgDiv.style.justifyContent = "center"
+        imgDiv.style.alignItems = "center"
+        imgDiv.textContent = "Loading..."
+        imgDiv.style.height = "400px"
+        imgDiv.style.marginTop = "20px"
+        imgDiv.style.backgroundSize = "contain"
+        imgDiv.style.backgroundRepeat = "no-repeat"
+        imgDiv.style.backgroundPosition = "center"
+        imgDiv.style.overflow = 'hidden'
+
+
+        //creating and styling randomQueBtn Button
+        const randomQueBtn = document.createElement('button')
+        randomQueBtn.innerHTML = "Solve Random Easy Que &#127919;"
+        randomQueBtn.style.textAlign = "center"
+        randomQueBtn.style.padding = "16px"
+        randomQueBtn.style.width = "100%"
+        randomQueBtn.style.backgroundColor = "rgb(255, 192, 30 , 0.7)"
+        randomQueBtn.style.color = "#fff"
+        randomQueBtn.style.marginTop = "10px"
+        randomQueBtn.style.border = "none"
+        randomQueBtn.style.borderRadius = "6px"
+        randomQueBtn.style.outline = "none"
+        randomQueBtn.style.fontSize = "1.2rem"
+        randomQueBtn.style.fontWeight = "600"
+        randomQueBtn.style.cursor = "pointer"
+        randomQueBtn.style.pointerEvents = 'auto'
+        randomQueBtn.onclick = () => {
+            goToRandomQue()
+        }
+
+        // Append HTML to Body
+        box.appendChild(header)
+        box.appendChild(imgDiv)
+        box.appendChild(randomQueBtn)
+        body.appendChild(box)
+
+
+    }
+
+}
 
 // Function returns username
 async function getUsername() {
@@ -48,3 +215,68 @@ async function getUsername() {
 
     })
 }
+
+//* Logic to redirect to random que
+async function goToRandomQue() {
+
+
+    const randomQueTitle = await fetch(`${serverProxy}/randomEasyQue`).catch(() => { alert('Error Occured During Redirecting') })
+    const res = await randomQueTitle.json()
+
+    randomQue = `https://leetcode.com/problems/${res}/`
+
+    window.open(randomQue)
+
+
+}
+
+//Function Injects HTML to show reminder
+async function showReminder() {
+
+
+    //Show the hidden Component
+    const leetcodeBox = document.getElementById('leetcodeBox')
+    leetcodeBox.style.transform = "translateX(0%)"
+    leetcodeBox.style.display = "block"
+
+    arr = [ 'Where you at ?' , 'Come on' , 'Let`s go' , 'What about Streaks ?' , 'Come on Vro we got a problem to solve' , 'Don`t forget to solve today' , 'Reminder !!!' , 'where is your determination vro ?' ]
+
+    let text =  encodeURIComponent (arr[ (Math.floor( Math.random () * 10000 ) % 10)%(arr.length) ])
+    
+    //Fecthing cat images
+    let dum = await fetch(`https://cataas.com/cat?type=sq&json=true`)
+    dum = await dum.json()
+
+
+
+
+    cat = `https://cataas.com/${(dum.url).split('?')[0]}/says/${text}?${(dum.url).split('?')[1]}`
+
+    console.log(cat);
+
+    //Assigning Cat Images to bg
+    const imgDiv = document.getElementById('catImgDiv')
+    imgDiv.textContent = null
+    imgDiv.style.backgroundImage = `url(${cat})`
+
+
+
+}
+
+//Function to hide the reminder
+async function hideReminder() {
+
+    const leetcodeBox = document.getElementById('leetcodeBox')
+    leetcodeBox.style.transform = "translateX(120%)"
+
+    setTimeout(() => {
+
+        () => {
+            leetcodeBox.style.display = "none"
+        }
+
+    }, 1200);
+
+
+}
+
