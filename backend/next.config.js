56375ec01c8c5e6691911af1850954338883694d
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-06-24 03:44:48 +0530

    updates on CORS src

--- backend/next.config.js
+++ backend/next.config.js
@@ -10,7 +10,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
