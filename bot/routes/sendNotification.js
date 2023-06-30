c41acfc8889df29b4f94d810b35fdab4b0c0b29f
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-06-30 17:53:49 +0530

    CORS updated

--- bot/routes/sendNotification.js
+++ bot/routes/sendNotification.js
@@ -12,12 +12,12 @@ dotenv.config()
 app.use(bodyParser.json());
 
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept , token");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept , token");
+//     next();
+// });
 
 
 app.post('/sendNotification', async (req, res) => {
