c05d6ae76e027403af5934d7e04ffef5b166af70
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-07-07 23:16:02 +0530

    Logic to send Email and Discord is done

--- bot/routes/data.js
+++ bot/routes/data.js
@@ -3,7 +3,7 @@ var app = express();
 const router = express.Router();
 
 const { initializeApp } = require("firebase/app");
-const { doc , getFirestore, collection, addDoc, setDoc, getDocs } = require("firebase/firestore");
+const { doc, getFirestore, collection, addDoc, setDoc, getDocs } = require("firebase/firestore");
 
 const FIREBASE_CONFIG = require('../firebase_config')
 
@@ -11,6 +11,9 @@ const firebaseConfig = FIREBASE_CONFIG;
 
 const server = initializeApp(firebaseConfig);
 const db = getFirestore(server);
+
+const { sendNotifications, updateJob } = require('../utils')
+const CronJob = require('cron').CronJob
 
 
 //To Update the data in database ( if username does not exists then it will create one )
@@ -25,7 +28,62 @@ router.post('/userInfo', async (req, res) => {
 
         setDoc(ref, data).then(() => {
 
-            res.status(200).send("Data Updates")
+
+            //If setTime is not defined then no need to create job
+            if (data.setTime === null || data.setTime === undefined) {
+                res.status(200).send("Data Updates")
+                return
+            }
+
+            //get currentTime in minutes
+            let currentTime = new Date().getHours() * 60 + new Date().getMinutes()
+
+            try {
+
+                let map = req.app.get('map')
+                let min = 0, hr = 0
+                let newSetTime = data.setTime
+
+                //If the setTime is already elapsed we cannot make scheduled job for that so we need to make shedule job for next possible time considering interval
+                if (data.setTime <= currentTime) {
+
+                    //new SetTime at which we wanna set the job
+                    newSetTime = currentTime + (data.interval - ((currentTime - data.setTime) % data.interval))
+
+
+                }
+
+                hr = Math.floor(newSetTime / 60)
+                min = newSetTime % 60
+
+                let time = `${min} ${hr} * * *`
+
+                console.log(` Job Scheduled for ${data.username} at ${time} `);
+
+                //Create a job for the new SetTime
+                let job = new CronJob(
+                    time,
+                    async function () {
+
+                        let client = req.app.get('client');
+                        sendNotifications(data.username, data.email, data.discordName, client)
+
+                        map[data.username].job.stop() // stop the current job
+                        await updateJob(data.username, hr, min, map, client) // update job
+
+                    },
+                    null,
+                    true,
+                );
+
+                res.status(200).send("Data Updates")
+            } catch (e) {
+                console.log(e);
+                res.status(500).send("data Added but job noy update")
+
+            }
+
+
 
         }).catch((e) => {
 
