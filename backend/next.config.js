d975f8162fe327bac641ffc2d28a7b32cc3bc405
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-09-16 10:38:09 +0530

    AccesesToken Added

--- backend/next.config.js
+++ backend/next.config.js
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
 module.exports = {
 
-  nextConfig,
+  reactStrictMode: true,
   async headers() {
     return [
       {
--- backend/pages/api/getUserDetails.js
+++ backend/pages/api/getUserDetails.js
@@ -1,42 +1,40 @@
 import { LeetCode } from "leetcode-query";
+let CryptoJS = require("crypto-js");
+const dotenv = require('dotenv').config()
 
 export default async function handler(req, res) {
+  //if GET method throws error
 
-
-        //if GET method throws error
-
-        if( req.method === "GET" ){
-
-            try{
-
-                throw new Error(' GET request are not allowed ')
-
-            }catch( err ){
-
-                res.status(500).json( err.message )
-
-            }
-
-        }
-    
-        //Finds the user and return the user information
-        if( req.method === "POST" ){
-
-            // console.log(req.body); //! debugging
-
-            try {
-                const leetcode = new LeetCode();
-                const user = await leetcode.user(req.body.username);
-
-                if( user.matchedUser === null ) throw new Error('User Not Found')
-
-                res.status(200).json(user.matchedUser)
-                
-            } catch (err) {
-                res.status(500).json(err.message)
-            }
-
-        }
-
+  if (req.method === "GET") {
+    try {
+      throw new Error(" GET request are not allowed ");
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
   }
-  
+
+  //Finds the user and return the user information
+  if (req.method === "POST") {
+    // console.log(req.body); //! debugging
+
+    try {
+      const leetcode = new LeetCode();
+      const user = await leetcode.user(req.body.username);
+
+      if (user.matchedUser === null) throw new Error("User Not Found");
+
+      // Encrypt
+      let accessToken = CryptoJS.AES.encrypt(
+        req.body.username,
+        process.env.SECRET_KEY
+      ).toString();
+
+      user.matchedUser['accessToken'] = accessToken;
+
+      res.status(200).json(user.matchedUser);
+
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  }
+}
